39d19ef0b174bbfed5a4e2070c4e9d4b9c5f5697
Tien Phan <tienphan1989@gmail.com>
2020-06-02 17:49:09 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        if @roster.key?(grade) == false
+        roster[grade] = []
+        #binding.pry
+        end
+        roster[grade].push(name)
+    end
+
+    def grade(grade)
+        roster[grade]  
+    end
+
+    def sort
+        roster.each do |grade, name|
+            roster[grade] = name.sort
+        end
+    end
+end
