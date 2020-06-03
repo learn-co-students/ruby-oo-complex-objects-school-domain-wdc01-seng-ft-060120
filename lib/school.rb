1dc10786a3c83424e4d122ed46496d62d2b6958e
estherkang14 <estherkang14@gmail.com>
2020-06-02 23:00:30 -0400

    finished learn lab and passed all tests. need better understanding though

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry' 
+
+class School
+
+    attr_reader :name
+    attr_accessor :roster 
+
+    def initialize(name)
+        @name = name
+        @roster = { } 
+    end 
+#roster will have key = grade; value = array of names
+    def add_student(name, grade)
+        if roster.key?(grade) == false
+            roster[grade] = [ ]
+            roster[grade] << name 
+        else 
+            roster[grade] << name
+        end 
+    end
+
+    def grade(grade)
+        roster[grade]
+    end 
+
+    def sort 
+        roster.each do |grade, students_in_grade| 
+        roster[grade] = students_in_grade.sort 
+      #  binding.pry 
+        end
+    end
+end
